5c3a78318dbbf97ce40eff9313afcb1a96bef328
Frans Lytzen <flytzen@neworbit.co.uk>
2019-06-29 23:13:37 +0100

    Initial map load

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,17 +1,28 @@
 import "./css/main.css";
+import {} from 'googlemaps';
 
-const divMessages: HTMLDivElement = document.querySelector("#divMessages");
-const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
-const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
-const username = new Date().getTime();
-
-tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
-    if (e.keyCode === 13) {
-        send();
+const googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBjUrTWAgKu6j_jhC3flP8S_TRXoaBPckU";
+function addMapsScript() {
+    if (!document.querySelectorAll(`[src="${googleMapsUrl}"]`).length) { 
+      document.body.appendChild(Object.assign(
+        document.createElement('script'), {
+          type: 'text/javascript',
+          src: googleMapsUrl,
+          onload: () => doMapInitLogic()
+        }));
+    } else {
+      this.doMapInitLogic();
     }
-});
+  };
 
-btnSend.addEventListener("click", send);
-
-function send() {
+function doMapInitLogic() {
+    console.log("in doMapInitLogic");
+    var map, infoWindow;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6
+    });
+    infoWindow = new google.maps.InfoWindow;
 }
+
+addMapsScript();
